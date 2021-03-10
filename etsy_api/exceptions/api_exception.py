c107801f7e4b094299a388b2abe46fffa695dfd0
run4w4y <add4che@gmail.com>
2021-03-10 16:08:32 +0000

    more insightful exceptions

--- etsy_api/exceptions/api_exception.py
+++ etsy_api/exceptions/api_exception.py
@@ -1,5 +1,29 @@
-class ApiException(BaseException):
+class ApiException(Exception):
     pass
 
 class MethodVisibilityException(ApiException):
+    pass
+
+# status code 400
+class BadRequestException(ApiException):
+    pass
+
+# status code 403
+class ForbiddenException(ApiException):
+    pass
+
+# status code 404
+class NotFoundException(ApiException):
+    pass
+
+# status code 500
+class ServerSideException(ApiException):
+    pass
+
+# status code 503
+class ServiceUnavailableException(ApiException):
+    pass
+
+# neither of the status codes above
+class UnknownApiException(ApiException):
     pass
--- etsy_api/types/api_method.py
+++ etsy_api/types/api_method.py
@@ -1,6 +1,7 @@
 import urllib
 import httpx
-from ..exceptions import MethodVisibilityException
+from ..exceptions import MethodVisibilityException, UnknownApiException, BadRequestException, ForbiddenException
+from ..exceptions import NotFoundException, ServerSideException, ServiceUnavailableException
 from ..api_key import EtsyApiKey
 from dataclasses import dataclass
 
@@ -31,6 +32,21 @@ class ApiMethod:
         args_encoded = urllib.parse.urlencode(request_args)
         async with httpx.AsyncClient() as client:
             r = await client.get(f'{self.base_url}{self.uri}?{args_encoded}', timeout=timeout)
-            r.raise_for_status() # TODO: [ ] should handle it in a better way
+        
+        if r.status_code not in [200, 201]:
+            exception_details = r.headers.get('X-Error-Detail')
+            exception_class = UnknownApiException
+            if r.status_code == 400:
+                exception_class = BadRequestException
+            elif r.status_code == 403:
+                exception_class = ForbiddenException
+            elif r.status_code == 404:
+                exception_class = NotFoundException
+            elif r.status_code == 500:
+                exception_class = ServerSideException
+            elif r.status_code == 503:
+                exception_class = ServiceUnavailableException
+            
+            raise exception_class(f'Got {r.status_code}. Error details: {exception_details}')
         
         return r.json()
